2e3eb9f18fb99c27624e5ac8d5be7430ad51daf1
FelipePy <felipespsampaiomotasp@gmail.com>
2024-03-20 10:45:06 -0300

    remove access_token on logout

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -65,7 +65,7 @@ export const AuthProvider = ({children}) => {
     const logout = async () => {
             await axios.post('https://api-nomades.onrender.com/auth/logout/',
                 {
-                    "access_token": "Bearer" + localStorage.getItem('access_token')
+                    "access_token": "Bearer " + localStorage.getItem('access_token')
                 }, {
                     'Content-Type': 'application/json'
                 }
