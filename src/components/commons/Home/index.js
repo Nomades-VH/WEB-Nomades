8e1fb9d334dd644a1f756799188b77d7f25d4864
FelipePy <felipespsampaiomotasp@gmail.com>
2024-03-22 15:16:01 -0300

    Testing deploy

--- src/components/commons/Home/index.js
+++ src/components/commons/Home/index.js
@@ -7,7 +7,7 @@ function importAll(r) {
     return r.keys().map(r);
 }
 
-const images = importAll(require.context("../../../public/exames/", false, '/\.(png|jpe?g|svg)$/'));
+const images = importAll(require.context("../../../public/exames/", false, /\.(png|jpe?g|svg)$/));
 
 
 export default function Menu(props) {
