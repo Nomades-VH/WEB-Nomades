56d49064c7af6eb76f28a7d4315d56aca60196eb
FelipePy <felipespsampaiomotasp@gmail.com>
2024-04-02 12:02:58 -0300

    no scape

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home(props) {
                             <p>
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
                                 been
-                                the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                the industrys standard dummy text ever since the 1500s, when an unknown printer took a
                                 galley
                                 of type and scrambled it to make a type specimen book. It has survived not only five
                                 centuries,
@@ -34,7 +34,7 @@ export default function Home(props) {
                                 Lorem Ipsum.
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
                                 been
-                                the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                the industrys standard dummy text ever since the 1500s, when an unknown printer took a
                                 galley
                                 of type and scrambled it to make a type specimen book. It has survived not only five
                                 centuries,
@@ -55,7 +55,7 @@ export default function Home(props) {
                                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
                                     has
                                     been
-                                    the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                    the industrys standard dummy text ever since the 1500s, when an unknown printer took a
                                     galley
                                     of type and scrambled it to make a type specimen book. It has survived not only five
                                     centuries,
@@ -69,7 +69,7 @@ export default function Home(props) {
                                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
                                     has
                                     been
-                                    the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                    the industrys standard dummy text ever since the 1500s, when an unknown printer took a
                                     galley
                                     of type and scrambled it to make a type specimen book. It has survived not only five
                                     centuries,
@@ -92,7 +92,7 @@ export default function Home(props) {
                             <p>
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
                                 been
-                                the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                the industrys standard dummy text ever since the 1500s, when an unknown printer took a
                                 galley
                                 of type and scrambled it to make a type specimen book. It has survived not only five
                                 centuries,
@@ -104,7 +104,7 @@ export default function Home(props) {
                                 Lorem Ipsum.
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
                                 been
-                                the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                the industrys standard dummy text ever since the 1500s, when an unknown printer took a
                                 galley
                                 of type and scrambled it to make a type specimen book. It has survived not only five
                                 centuries,
