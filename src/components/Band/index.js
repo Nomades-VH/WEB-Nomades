f8ed1945471c17ab034f313847fb53b412ef6922
FelipePy <felipespsampaiomotasp@gmail.com>
2024-04-25 10:17:01 -0300

    Remove 'complemento' from component Band

--- src/components/Band/index.js
+++ src/components/Band/index.js
@@ -7,13 +7,13 @@ export default function Band({band, poomsaes, kicks, kibon_donjaks}) {
 
             <div className={styles.container}>
                 <h1>{band.gub}° - {band.name}</h1>
-                <p>{band.meaning}</p>
+                <p style={{alignSelf: "center"}}>{band.meaning}</p>
                 <div className={styles.content}>
 
                     <h3>1° - Kibon Donjaks</h3>
                     {kibon_donjaks.map((kibon_donjak, index) => {
                         return (
-                            <div key={index} className={styles.attributeBox}>
+                            <div key={index}>
                                 <div className={styles.attribute}>
                                     <h4>{index + 1}º - <a href="#" key={index} className="link">{kibon_donjak.name}</a>
                                     </h4>
@@ -26,7 +26,7 @@ export default function Band({band, poomsaes, kicks, kibon_donjaks}) {
                     <h3>2° - Poomsaes</h3>
                     {poomsaes.map((poomsae, index) => {
                         return (
-                            <div key={index} className={styles.attributeBox}>
+                            <div key={index}>
                                 <div className={styles.attribute}>
                                     <h4>{index + 1}° - <a href='#' className="link" key={index}>{poomsae.name}</a></h4>
                                 </div>
@@ -37,7 +37,7 @@ export default function Band({band, poomsaes, kicks, kibon_donjaks}) {
                     <h3>3° - Chutes</h3>
                     {kicks.map((kick, index) => {
                         return (
-                            <div key={index} className={styles.attributeBox}>
+                            <div key={index}>
                                 <div className={styles.attribute}>
                                     <h4>{index + 1}º - <a href="#" className="link" key={index}>{kick.name}</a></h4>
                                     <p key={index}>({kick.description})</p>
@@ -60,9 +60,9 @@ export default function Band({band, poomsaes, kicks, kibon_donjaks}) {
                     </div>
                 </div>
             </div>
-            <div className={styles.container}>
-                <h1>Complemento</h1>
-            </div>
+            {/*<div className={styles.container}>*/}
+            {/*    <h1>Complemento</h1>*/}
+            {/*</div>*/}
         </div>
     )
 
