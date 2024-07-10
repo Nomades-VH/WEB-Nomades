c903266fda3e579be1e904ddf33bc3a981ae921c
FelipePy <felipespsampaiomotasp@gmail.com>
2024-07-10 15:52:56 -0300

    Adding get_by_id for kicks

---
+++ src/pages/kick/[id].js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import KickService from "../../services/kick";
+import Loading from "../../components/commons/Loading";
+import { useNavigate} from "react-router-dom";
+import DisplayPage from "../../components/DisplayPage";
+
+export default function Kick() {
+    const {id} = useParams();
+    const [kick, setKick] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (id) {
+            const loadKick = async (id) => {
+                const response = await KickService.get_by_id(id);
+
+                if (response) {
+                    setKick(response)
+                } else {
+                    navigate('/chute')
+                }
+            }
+            
+            loadKick(id)
+        }
+    }, [id])
+
+
+    if (kick) {
+        return (
+            <DisplayPage titlePage={<h1>{kick.name}</h1>}>
+                <p>{kick.description}</p>
+            </DisplayPage>
+        )
+    } return <Loading />
+}
