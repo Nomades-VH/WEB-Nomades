b43010a0bd55186d137043df5e38ce06360e6e57
FelipePy <felipespsampaiomotasp@gmail.com>
2024-08-27 14:50:30 -0300

    update base url

--- src/services/base.js
+++ src/services/base.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AuthService from "./auth";
 
 export const instance = axios.create({
-	baseURL: 'http://localhost:8000',
+	baseURL: 'https://api-nomades.onrender.com',
     paramsSerializer: {
         indexes: null,
     },
